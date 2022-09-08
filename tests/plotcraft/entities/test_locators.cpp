00418cb11914d684771c0216ebfc61f663e3cda3
Christian Gröling <ch.groeling@gmail.com>
2022-09-08 11:59:33 +0200

    Fixed: Locator unit test

--- tests/plotcraft/entities/test_locators.cpp
+++ tests/plotcraft/entities/test_locators.cpp
@@ -9,25 +9,25 @@ using ::testing::ElementsAre;
 using namespace plotcraft::entities;
 
 TEST(TestMaxNLocator, TestGetTickValuesInterval0To05VectorWith6Elements) {
-    MaxNLocator max_n_loc(9);
+  MaxNLocator max_n_loc(9);
 
-    auto res = max_n_loc.GetTickValues(0.0, 0.5);
-    ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.1), DoubleEq(0.2),
-                                 DoubleEq(0.3), DoubleEq(0.4), DoubleEq(0.5)));
+  auto res = max_n_loc.GetTickValues(0.0, 0.5);
+  ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.1), DoubleEq(0.2), DoubleEq(0.3),
+                               DoubleEq(0.4), DoubleEq(0.5)));
 }
 
 TEST(TestMaxNLocator, TestGetTickValuesIntervalm005To055VectorWith6Elements) {
-    MaxNLocator max_n_loc(9);
+  MaxNLocator max_n_loc(9);
 
-    auto res = max_n_loc.GetTickValues(-0.05, 0.55);
-    ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.1), DoubleEq(0.2),
-                                 DoubleEq(0.3), DoubleEq(0.4), DoubleEq(0.5)));
+  auto res = max_n_loc.GetTickValues(-0.05, 0.55);
+  ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.1), DoubleEq(0.2), DoubleEq(0.3),
+                               DoubleEq(0.4), DoubleEq(0.5)));
 }
 
-TEST(TestMaxNLocator, TestGetTickValuesInterval) {
-    MaxNLocator max_n_loc(9);
+TEST(TestMaxNLocator, TestGetTickValuesInterval0To01p3VectorWith6Elements) {
+  MaxNLocator max_n_loc(9);
 
-    auto res = max_n_loc.GetTickValues(0.0, 1.3);
-    ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.1), DoubleEq(0.2),
-                                 DoubleEq(0.3), DoubleEq(0.4), DoubleEq(0.5)));
+  auto res = max_n_loc.GetTickValues(0.0, 1.3);
+  ASSERT_THAT(res, ElementsAre(DoubleEq(0.0), DoubleEq(0.25), DoubleEq(0.5), DoubleEq(0.75),
+                               DoubleEq(1.0), DoubleEq(1.25)));
 }
